a578304ad2cc4f46f772953c849789442e117f24
ElKebaboKing <ranielmaalouf@gmail.com>
2017-05-16 21:19:49 +0200

    Create TextViews for all the numbers and add them to the root view from the java code.ss

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,7 +17,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,6 +35,19 @@ public class NumbersActivity extends AppCompatActivity {
         // Create array of words
         ArrayList<String> words = new ArrayList<String>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
 
-        
+        // Find the correct root view
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for(int i = 0; i < words.size(); i++)
+        {
+            // Create a TextView
+            TextView wordView = new TextView(this);
+
+            // Write the correct text to the new TextView
+            wordView.setText(words.get(i));
+
+            // Add the new TextView to the root view
+            rootView.addView(wordView);
+        }
     }
 }
